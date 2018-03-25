7a0f11835ad091895a793b341c2af5bed93380e5
AveRomain <33924524+AveRomain@users.noreply.github.com>
2018-03-25 22:08:29 +0200

    Ajout méthode ajouter + supprimer (problème)

--- src/src/ListeArcs.cpp
+++ src/src/ListeArcs.cpp
@@ -1,3 +1,26 @@
 #include "../include/ListeArcs.h"
 
 ListeArcs::ListeArcs() {}
+
+void ListeArcs::ajouter(Arc info, Sommet* successeur) {
+    ListeArcs* courant = d_suivant;
+    ListeArcs nouveau = new ListeArcs();
+    nouveau->d_info = info;
+    nouveau->d_successeur = successeur;
+
+    while(courant->d_suivant != nullptr) {
+        courant = courant->d_suivant;
+    }
+    courant->d_suivant = nouveau;
+}
+
+void ListeArcs::supprimer(Arc info, Sommet* successeur) {
+    if(this != nullptr) {
+        if(d_info == info && d_successeur == successeur) {
+            ListeArcs* courant = d_suivant;
+        }
+    }
+    while(courant->d_info != info || courant->d_successeur != successeur && courant->d_suivant != nullptr) {
+        courant = courant->d_suivant;
+    }
+}
